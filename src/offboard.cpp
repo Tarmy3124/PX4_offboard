80dd522ecbed411627b030d6ce78325e21b1dcd0
Tarmy3124 <1498860484@qq.com>
2020-10-20 05:46:53 -0700

    模式切换流程正常，接收字符串指令

--- src/offboard.cpp
+++ src/offboard.cpp
@@ -10,16 +10,28 @@
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 #include <rosmsg_template/simple.h>
+#include <string>
 using namespace std;
-char getkey;
+const unsigned char *pregetkey;
+char getkey[20]={};
+string keystring;
 mavros_msgs::State current_state;
-void keyvalue_cb(const rosmsg_template::simple::ConstPtr& msg)
+void keyvalue_cb(const rosmsg_template::simple::ConstPtr &msg)
 {
- cout<<"key value is "<<msg->keychar<<endl;
-getkey =msg->keychar;
+// cout<<"key value is "<<msg->keystring<<endl;
+//strcpy(getkey,&msg.keystring[0]);
+pregetkey=&msg->keystring[0];
+//for循环转换格式
+for(int i=0;i<sizeof(pregetkey);i++)
+{
+ getkey[i]=pregetkey[i];
+}
+//keystring =(string *)&msg->keystring[0];
+cout<<"what i get "<<getkey<<endl;
 }
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
+    cout<<"Px4 current state is "<<current_state.mode<<endl;
 }
 
 int main(int argc, char **argv)
@@ -38,6 +50,8 @@ int main(int argc, char **argv)
             ("mavros/set_mode"); // 创建一个客户端来设置模式
     ros::ServiceClient set_submode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode"); // 创建一个客户端来降落
+        ros::ServiceClient set_submode_loiter = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode"); // 创建一个客户端来loiter
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
@@ -61,8 +75,9 @@ int main(int argc, char **argv)
         rate.sleep(); // 保证更新频率为20Hz，自动调整睡眠时间
     }
 
-    mavros_msgs::SetMode offb_set_mode,land_set_mode;
+    mavros_msgs::SetMode offb_set_mode,land_set_mode,loiter_set_mode;
     land_set_mode.request.custom_mode = "AUTO.LAND";
+    loiter_set_mode.request.custom_mode = "AUTO.LOITER";
     offb_set_mode.request.custom_mode = "OFFBOARD"; // 调用者请求
 
     mavros_msgs::CommandBool arm_cmd;
@@ -72,11 +87,21 @@ int main(int argc, char **argv)
 
     while(ros::ok()){
       
-          if(getkey == 'l'){
+          if(strncmp(getkey,"land",strlen("land"))==0){
+            strcpy(getkey , "nothing to do"); //for null
              if( set_submode_client.call(land_set_mode) && land_set_mode.response.mode_sent){ // 被调用者响应
                 ROS_INFO("landing ....");
             }
          }
+         if(strncmp(getkey,"loiter", strlen("loiter"))==0){ //loiter
+             strcpy(getkey , "nothing to do"); //for null
+             if( set_submode_loiter.call(loiter_set_mode) && loiter_set_mode.response.mode_sent){ // 被调用者响应
+                ROS_INFO("loiter ....");
+            }
+         }
+     if(strncmp(getkey,"offboard", strlen("offboard"))==0){
+        strcpy(getkey,"nothing to do");
+        ROS_INFO("Offboard ready to takeoff");
         if( current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
             if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent){ // 被调用者响应
@@ -91,9 +116,9 @@ int main(int argc, char **argv)
                     ROS_INFO("Vehicle armed");
                 }
                 last_request = ros::Time::now();
-            }
         }
-
+      }
+    }
         local_pos_pub.publish(pose);
 //ros::spin();
         ros::spinOnce();
